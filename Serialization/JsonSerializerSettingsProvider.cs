6fb35460125058562bb6ace45e562e04ef827bcc
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-07 22:34:32 +0300

    Added json settings provider to use the same settings in all cases

---
+++ Serialization/JsonSerializerSettingsProvider.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ClimateMeter.Web.Serialization
+{
+    public static class JsonSerializerSettingsProvider
+    {
+        public static JsonSerializerSettings CreateSettings()
+        {
+            var settings = new JsonSerializerSettings();
+            
+            SetSettings(settings);
+            
+            return settings;
+        }
+
+        public static void SetSettings(JsonSerializerSettings settings)
+        {
+            settings.NullValueHandling = NullValueHandling.Include;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.TypeNameHandling = TypeNameHandling.None;
+        }
+    }
+}
