d9ec593cd61a20b4419e6e0aa6cd52401b3c7b73
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-18 02:50:17 +0300

    Added database models

---
+++ DAL/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClimateMeter.Web.DAL
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public Guid DeviceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<SensorReading> SensorReadings { get; set; }
+    }
+}
---
+++ DAL/SensorReading.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClimateMeter.Web.DAL
+{
+    public class SensorReading
+    {
+        public int Id { get; set; }
+        public Guid DeviceId { get; set; }
+        public Device Device { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public DateTimeOffset ReceivedOn { get; set; }
+    }
+}
