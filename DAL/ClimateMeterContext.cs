96b6d9ade0f87e0d40e9de8999926678274bc374
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-18 02:50:42 +0300

    Added db context with model creating logic

---
+++ DAL/ClimateMeterContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClimateMeter.Web.DAL
+{
+    public class ClimateMeterContext: DbContext
+    {
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<SensorReading> SensorReadings { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var deviceBuilder = modelBuilder.Entity<Device>();
+            deviceBuilder.HasKey(d => d.Id);
+            deviceBuilder.HasAlternateKey(d => d.Name);
+            deviceBuilder.Property(d => d.DeviceId).ValueGeneratedOnAdd();
+            deviceBuilder.Property(d => d.Name).IsRequired().HasMaxLength(260);
+            deviceBuilder.Property(d => d.Description).IsRequired();
+
+            var sensorReadingBuilder = modelBuilder.Entity<SensorReading>();
+            sensorReadingBuilder.HasKey(r => r.Id);
+            sensorReadingBuilder
+                .HasOne(r => r.Device)
+                .WithMany(d => d.SensorReadings)
+                .HasForeignKey(r => r.DeviceId)
+                .HasPrincipalKey(d => d.DeviceId);
+        }
+    }
+}
