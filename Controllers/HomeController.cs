f989ae38a063f041228a5effcb3af3689b8b3721
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-18 16:50:16 +0300

    Added sensor readings endpoint for testing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using ClimateMeter.Web.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClimateMeter.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ClimateMeterContext _dbContext;
+
+        public HomeController(ClimateMeterContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("sensorreadings")]
+        public async Task<IActionResult> SensorReadings()
+        {
+            var readings = await _dbContext.SensorReadings.ToArrayAsync();
+            return Ok(readings);
         }
     }
 }
