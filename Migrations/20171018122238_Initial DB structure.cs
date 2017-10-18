ccd2745ffbc70052929d50398f788eb8179d9f68
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-18 15:47:47 +0300

    Added Initial Structure migration

---
+++ Migrations/20171018122238_Initial DB structure.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ClimateMeter.Web.Migrations
+{
+    public partial class InitialDBstructure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(260)", maxLength: 260, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.UniqueConstraint("AK_Devices_DeviceId", x => x.DeviceId);
+                    table.UniqueConstraint("AK_Devices_Name", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorReadings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Humidity = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    ReceivedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Temperature = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorReadings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SensorReadings_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "DeviceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorReadings_DeviceId",
+                table: "SensorReadings",
+                column: "DeviceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SensorReadings");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
