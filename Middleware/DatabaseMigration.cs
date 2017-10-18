e9751ce475016ff4f991a44134ff8fa3f3a8d8a2
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-18 15:48:14 +0300

    Added middleware to ensure that db is migrated on application start

---
+++ Middleware/DatabaseMigration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClimateMeter.Web.Middleware
+{
+    public static class DatabaseMigrationExtensions
+    {
+        public static IApplicationBuilder EnsureMigrationsApplied<T>(this IApplicationBuilder app) where T:DbContext
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<T>();
+                context.Database.Migrate();
+            }
+
+            return app;
+        }
+    }
+}
