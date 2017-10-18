5e2c6c14b69526b1f4d0a81880e4d6981d5c7e5e
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-18 17:48:05 +0300

    Added environment variables to be able to connect to consul on the vnet

--- setup_climatemeter_web.sh
+++ setup_climatemeter_web.sh
@@ -3,6 +3,8 @@
 echo "Starting ClimateMeter.Web..."
 sudo docker run --name web \
     --net dockernet \
+    -e CONSUL_HOST=consul \
+    -e CONSUL_PORT=8500 \
     -e VIRTUAL_HOST=climatemeter.darkxahtep.co.ua \
     -e VIRTUAL_PORT=5000 \
     -e LETSENCRYPT_HOST=climatemeter.darkxahtep.co.ua \
