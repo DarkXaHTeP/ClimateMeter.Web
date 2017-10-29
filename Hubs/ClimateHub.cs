872e430a01b66ef8ed0161980793736bbbb56e13
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-29 03:00:33 +0300

    Updated hub methods names

--- Hubs/ClimateHub.cs
+++ Hubs/ClimateHub.cs
@@ -14,13 +14,13 @@ namespace ClimateMeter.Web.Hubs
             _db = db;
         }
         
-        public async Task OnRegisterDevice(string name, string description)
+        public async Task RegisterDevice(string name, string description)
         {
             Console.WriteLine($"registration requested: name - {name}, description - {description}");
-            await Clients.Client(Context.ConnectionId).InvokeAsync("OnDeviceRegistered", Guid.NewGuid());
+            await Clients.Client(Context.ConnectionId).InvokeAsync("DeviceRegistered", Guid.NewGuid());
         }
 
-        public void OnSensorReading(Guid id, decimal temperature, decimal humidity)
+        public void AddSensorReading(Guid id, decimal temperature, decimal humidity)
         {
             Console.WriteLine($"Data received from {id}, temp = {temperature}, humidity = {humidity}");
         }
