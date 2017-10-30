44dc31d214ebc819434763337509483c41552aec
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-30 02:41:57 +0200

    Added authorization settings type

---
+++ Authorization/AuthorizationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ClimateMeter.Web.Authorization
+{
+    public class AuthorizationSettings
+    {
+        public string Instance { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+    }
+}
