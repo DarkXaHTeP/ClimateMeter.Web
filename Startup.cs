881f6cab17353e5572f0d75d495cc9a12a236f8a
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-30 02:42:57 +0200

    Added JWTBearer auth to app

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
-﻿using ClimateMeter.Web.DAL;
+﻿using ClimateMeter.Web.Authorization;
+using ClimateMeter.Web.DAL;
 using ClimateMeter.Web.Hubs;
 using ClimateMeter.Web.Middleware;
 using ClimateMeter.Web.Serialization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -21,7 +24,20 @@ namespace ClimateMeter.Web
         public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = _configuration.GetConnectionString("ClimateMeter.DB");
+            var authorizationSettings = _configuration.GetSection("Authorization").Get<AuthorizationSettings>();
 
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = $"{authorizationSettings.Instance}/{authorizationSettings.TenantId}";
+                    options.Audience = authorizationSettings.ClientId;
+                });
+            
             services.AddDbContext<ClimateMeterContext>(options => options.UseSqlServer(connectionString));
             services.AddSignalR();
             services.AddMvc()
@@ -31,6 +47,7 @@ namespace ClimateMeter.Web
         public void Configure(IApplicationBuilder app)
         {
             app
+                .UseAuthentication()
                 .UseDeveloperExceptionPage()
                 .EnsureMigrationsApplied<ClimateMeterContext>()
                 .UseStaticFiles()
