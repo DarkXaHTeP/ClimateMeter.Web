68deb06ccd3d91342905cf2b77d4488a134625f4
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-31 22:27:38 +0200

    Added signalr token parsing options

---
+++ Hubs/SignalRJwtParsingOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace ClimateMeter.Web.Hubs
+{
+    public class SignalRJwtParsingOptions
+    {
+        public string TokenQueryStringParameter { get; set; }
+        public string JwtAuthenticationSchemeName { get; set; }
+    }
+}
