4740db018c081d3c6346a977723cd4944cebac3a
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-25 14:42:11 +0200

    Read file in using block to possibly eliminate issue with file not written to output

--- Controllers/FrontEndController.cs
+++ Controllers/FrontEndController.cs
@@ -1,21 +1,34 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using ClimateMeter.Web.DAL;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 
 namespace ClimateMeter.Web.Controllers
 {
     public class FrontEndController : Controller
     {
         private readonly ClimateMeterContext _dbContext;
+        private readonly IHostingEnvironment _env;
 
-        public FrontEndController(ClimateMeterContext dbContext)
+        public FrontEndController(ClimateMeterContext dbContext, IHostingEnvironment env)
         {
             _dbContext = dbContext;
+            _env = env;
         }
         
         public IActionResult Index()
         {
-            return File("~/index.html", "text/html");
+            IFileInfo file = _env.WebRootFileProvider.GetFileInfo("index.html");
+
+            using (var stream = file.CreateReadStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return Content(reader.ReadToEnd(), "text/html");
+                }
+            }
         }
 
         [Route("sensorreadings")]
