af9d71200443fdd7a675e598832e6ba869656b00
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-25 21:23:04 +0200

    Added home component

--- UI/app/app.module.ts
+++ UI/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-
+import { routes } from "./app.routes";
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+      AppComponent
+  ]
 })
 export class AppModule { }
