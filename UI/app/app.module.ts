73d4c7a7ceaf38edf04db1e11444655d715c478d
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-25 22:00:04 +0200

    Added displaying of loaded devices to home page

--- UI/app/app.module.ts
+++ UI/app/app.module.ts
@@ -1,24 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
-import { routes } from "./app.routes";
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home';
+import {routes} from "./app.routes";
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home';
+import { DeviceApiService } from "./api";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [],
-  bootstrap: [
-      AppComponent
-  ]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [
+        DeviceApiService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- UI/app/home/home.component.ts
+++ UI/app/home/home.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DeviceApiService, Device} from "../api";
+import "rxjs/add/operator/do";
 
 @Component({
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    isLoading: boolean = false;
+    devices: Device[] = [];
+    error: string;
+  
+    constructor(private deviceApi: DeviceApiService) {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.isLoading = true;
+        this.deviceApi
+            .listDevices()
+            .do(() => this.isLoading = false)
+            .subscribe(
+                (devices) => {
+                    this.devices = devices;
+                },
+                (error) => {
+                    this.isLoading = false;
+                    this.error = error;
+                }
+            )
+    }
 }
