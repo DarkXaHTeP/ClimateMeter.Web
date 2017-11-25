3728d8ed87e0bd6377c39e2e7a14b1ddc8bab1ef
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-25 22:28:15 +0200

    Added devices api endpoint

---
+++ Controllers/Api/DevicesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using ClimateMeter.Web.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClimateMeter.Web.Controllers.Api
+{
+    [Route("api/devices")]
+    public class DevicesController : Controller
+    {
+        private readonly ClimateMeterContext _db;
+
+        public DevicesController(ClimateMeterContext db)
+        {
+            _db = db;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var devices = await _db.Devices.ToListAsync();
+            return Ok(devices);
+        }
+    }
+}
